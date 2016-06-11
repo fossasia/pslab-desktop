7827a859d8010cbfac7060190a973d3385c7fead
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2016-06-12 00:54:31 +0530

    Create template_diode.py

---
+++ PSL_Apps/templates/template_diode.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'diode.ui'
+
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(216, 338)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        Form.setStyleSheet(_fromUtf8(""))
+        self.verticalLayout = QtGui.QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.frame = QtGui.QFrame(Form)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame.sizePolicy().hasHeightForWidth())
+        self.frame.setSizePolicy(sizePolicy)
+        self.frame.setStyleSheet(_fromUtf8(""))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.formLayout = QtGui.QFormLayout(self.frame)
+        self.formLayout.setObjectName(_fromUtf8("formLayout"))
+        self.progress = QtGui.QProgressBar(self.frame)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.progress.sizePolicy().hasHeightForWidth())
+        self.progress.setSizePolicy(sizePolicy)
+        self.progress.setStyleSheet(_fromUtf8(""))
+        self.progress.setProperty("value", 0)
+        self.progress.setObjectName(_fromUtf8("progress"))
+        self.formLayout.setWidget(0, QtGui.QFormLayout.SpanningRole, self.progress)
+        self.pushButton = QtGui.QPushButton(self.frame)
+        self.pushButton.setMinimumSize(QtCore.QSize(0, 50))
+        self.pushButton.setStyleSheet(_fromUtf8(""))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.formLayout.setWidget(1, QtGui.QFormLayout.SpanningRole, self.pushButton)
+        self.frame_2 = QtGui.QFrame(self.frame)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.frame_2.sizePolicy().hasHeightForWidth())
+        self.frame_2.setSizePolicy(sizePolicy)
+        self.frame_2.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame_2.setObjectName(_fromUtf8("frame_2"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.frame_2)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.VLabel = QtGui.QLabel(self.frame_2)
+        self.VLabel.setObjectName(_fromUtf8("VLabel"))
+        self.horizontalLayout.addWidget(self.VLabel)
+        self.ILabel = QtGui.QLabel(self.frame_2)
+        self.ILabel.setObjectName(_fromUtf8("ILabel"))
+        self.horizontalLayout.addWidget(self.ILabel)
+        self.formLayout.setWidget(2, QtGui.QFormLayout.SpanningRole, self.frame_2)
+        self.verticalLayout.addWidget(self.frame)
+
+        self.retranslateUi(Form)
+        QtCore.QObject.connect(self.pushButton, QtCore.SIGNAL(_fromUtf8("clicked(bool)")), Form.start)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtGui.QApplication.translate("Form", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("Form", "Sweep Voltage(PV2)", None, QtGui.QApplication.UnicodeUTF8))
+        self.VLabel.setText(QtGui.QApplication.translate("Form", "V=", None, QtGui.QApplication.UnicodeUTF8))
+        self.ILabel.setText(QtGui.QApplication.translate("Form", "I=", None, QtGui.QApplication.UnicodeUTF8))
+
