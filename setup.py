a13534c95acf7c917933b9e913f9925c81d0a108
Praveen (Gnovi) <wavicles@users.noreply.github.com>
2016-06-17 18:28:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ class CustomInstall(install):
 
 setup(name='PSL_Apps',
 	version='1.0.0',
-  description='GUIs for Experiments with PSLab. Requires PSL',
-  author='Jithin B.P. and Praveen Patil',
+  	description='GUIs for Experiments with PSLab. Requires PSL',
+  	author='Jithin B.P. and Praveen Patil',
 	install_requires = ['numpy>=1.8.1','pyqtgraph>=0.9.10'], #PSL>=
 	packages=['PSL_Apps','PSL_Apps.templates','PSL_Apps.utilityApps','PSL_Apps.utilityApps.templates','PSL_Apps.stylesheets','PSL_Apps.templates.widgets'],
 	scripts=["PSL_Apps/bin/"+a for a in os.listdir("PSL_Apps/bin/")],
