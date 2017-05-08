c720f2dc8fb5b922a77a586e7db523b1ffc31ae0
jithin <jithinbp@gmail.com>
2017-05-08 13:50:21 +0530

    FIX DUST_SENSOR.py (#97)
    * Fixed sensorpendulum app in the Physics section
    
    Made compatible with non-blocking calls defined in PSL.Peripherals.I2C .
    Bug fixes in template file.
    included saveData button to UI
    Fixes #93
    
    * code quality edits based on Codacy feedback
    
    changes to Y_sensorpendulum.py
    
    * Configure Travis to autocompile template files
    
     Changes to travis.yml , B_SpeedOfSound.py, J_rodpend.py, N_pendulum.py
    
    * Configure Travis to autocompile template files
    
    Changes to travis.yml , B_SpeedOfSound.py, J_rodpend.py, N_pendulum.py. partly fixes #95
    Travis checks against Python versions 2.7,3.3,3.4 . 3.2 build is failing for an as yet unidentified reason, and has temporarily been disabled.
    Fixes #95
    
    * code quality edits based on Codacy feedback
    
    changes to Y_sensorpendulum.py
    
    * Configure Travis to autocompile template files
    
    Changes to travis.yml , B_SpeedOfSound.py, J_rodpend.py, N_pendulum.py. partly fixes #95
    Travis checks against Python versions 2.7,3.3,3.4 . 3.2 build is failing for an as yet unidentified reason, and has temporarily been disabled.
    
    * edited Makefile. partly fixes #95
    
    * fix DUST_SENSOR.py . Add screenshot & help file
    
     fixes #96 . template changed to ui_dsm501a , indoors dust level screenshot added, help file included

--- psl_res/GUI/E_MISCELLANEOUS/A_Add-ons/DUST_SENSOR.py
+++ psl_res/GUI/E_MISCELLANEOUS/A_Add-ons/DUST_SENSOR.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python
 
-"""
-
-::
-
-    This experiment is used to study Half wave rectifiers
-
-"""
-
 from __future__ import print_function
 from PSL_Apps.utilitiesClass import utilitiesClass
 
-from .templates import dsm501
+from templates import ui_dsm501 as dsm501
 
 import numpy as np
 from PyQt4 import QtGui,QtCore
@@ -36,32 +28,43 @@ class AppWindow(QtGui.QMainWindow, dsm501.Ui_MainWindow,utilitiesClass):
 		labelStyle = {'color': 'rgb(255,255,255)', 'font-size': '11pt'}
 		self.plot1.setLabel('bottom','Time -->', units='S',**labelStyle)
 		self.plot1.getAxis('left').setLabel('Concentration -->>', color='#ffffff')
+		self.plot1.setLimits(xMin=0,yMin=0)
 
 		self.total_samples = 100
 		self.acquired_samples = 0
 		self.timegap = 10 #mS
-		self.sampling_time = 10000 #mS
+		self.sampling_time = 2000 #mS
 		
 		self.timer2 = QtCore.QTimer()
 		self.timer2.timeout.connect(self.updateProgress)
 		self.timer2.start(500)
+		self.I.set_state(SQR1=True)
 		
 		self.curve = self.addCurve(self.plot1,'Concentration')
 		self.resultsTable.setRowCount(self.total_samples)
 		self.resultsTable.setColumnCount(3)
-		self.resultsTable.setHorizontalHeaderLabels(['time','Occupancy','Concentration'])
+		self.resultsTable.setHorizontalHeaderLabels(['time','Ratio %','Concentration mg/m^3'])
 
 		self.running=False
 		self.start_time = time.time()
 		self.samplingStartTime=time.time()
 
-		self.timer = QtCore.QTimer()
-		self.running=True
-		self.timer.singleShot(0,self.run)
+		self.timer = self.newTimer()
+		#self.running=True
+		#self.timer.singleShot(0,self.run)
 		
 		self.X=[]
 		self.Y=[]
 
+	def start(self):
+		self.X=[]
+		self.Y=[]
+		self.running = True
+		self.timer.singleShot(0,self.run)
+
+	def stop(self):
+		self.running=False
+		
 	def updateProgress(self):
 		if not self.running:return
 		val = 1e5*(time.time()-self.samplingStartTime)/(self.sampling_time)
@@ -70,6 +73,7 @@ class AppWindow(QtGui.QMainWindow, dsm501.Ui_MainWindow,utilitiesClass):
 	def run(self):
 		if not self.running:return
 		self.samplingStartTime = time.time()
+		self.sampling_time = self.integrationBox.value()*1e3 #convert to mS
 		self.I.start_one_channel_LA(channel='ID1',channel_mode=1,trigger_mode=0)  #every edge
 		if self.running: self.timer.singleShot(self.sampling_time,self.plotData)
 
@@ -78,6 +82,7 @@ class AppWindow(QtGui.QMainWindow, dsm501.Ui_MainWindow,utilitiesClass):
 		a,b,c,d,e = self.I.get_LA_initial_states()
 		if a==self.I.MAX_SAMPLES/4:	a = 0
 		tmp = self.I.fetch_long_data_from_LA(a,1)
+		print (a,b,c,d,e,tmp)
 		self.I.dchans[0].load_data(e,tmp)
 		#print (self.I.dchans[0].timestamps,self.I.dchans[0].initial_state)
 		stamps = self.I.dchans[0].timestamps
@@ -88,11 +93,11 @@ class AppWindow(QtGui.QMainWindow, dsm501.Ui_MainWindow,utilitiesClass):
 			lows = diff[::2]
 			highs = diff[1::2]
 			#print(stamps,sum(lows),sum(highs))
-			low_occupancy = 100*sum(lows)/stamps[-1]
+			low_occupancy = 100*sum(lows)/stamps[-1] #Occupancy ratio
 			self.progressBar.setValue(low_occupancy)
-			concentration = 1.1*pow(low_occupancy,3)-3.8*pow(low_occupancy,2)+520*low_occupancy+0.62;
+			concentration = 1.1*pow(low_occupancy,3)-3.8*pow(low_occupancy,2)+520*low_occupancy+0.62; #From the spec sheet curve
 			self.X.append(time.time()-self.start_time)
-			self.Y.append(low_occupancy)
+			self.Y.append(concentration)
 			self.curve.setData(self.X,self.Y)
 			item = QtGui.QTableWidgetItem();item.setText('%s'%(time.strftime("%H:%M:%S %d-%h")));self.resultsTable.setItem(self.acquired_samples, 0, item);#item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEnabled)
 			item = QtGui.QTableWidgetItem();item.setText('%.3f'%(low_occupancy));self.resultsTable.setItem(self.acquired_samples, 1, item);#item.setFlags(QtCore.Qt.ItemIsSelectable|QtCore.Qt.ItemIsEnabled)
@@ -103,8 +108,8 @@ class AppWindow(QtGui.QMainWindow, dsm501.Ui_MainWindow,utilitiesClass):
 				self.resultsTable.setRowCount(self.total_samples)
 		if self.running: self.timer.singleShot(self.timegap,self.run)
 
-	def saveFile(self):
-		pass
+	def saveData(self):
+		self.saveDataWindow([self.curve],self.plot1)
 		
 	def closeEvent(self, event):
 		self.timer.stop()
@@ -114,7 +119,6 @@ class AppWindow(QtGui.QMainWindow, dsm501.Ui_MainWindow,utilitiesClass):
 
 	def __del__(self):
 		self.timer.stop()
-		print ('bye')
 
 if __name__ == "__main__":
     from PSL import sciencelab
