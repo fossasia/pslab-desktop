a59595acad41c629dc30765c6e0e058c8b609bb8
jithin <jithinbp@gmail.com>
2017-07-22 01:16:19 +0530

    Virtual Lab sign up process (#169)
    * added a sign-up page & css, and appropriate hook in views.py
    
    fixes #166, WIP #164
    
    * added sign-up functionality. frontend sends data via AJAX, and a mysql procedure is called by the backend to try and create a new user with the supplied credentials
    
     fixes #167 , WIP #164

--- virtual/virtualapp/views.py
+++ virtual/virtualapp/views.py
@@ -1,22 +1,24 @@
 from virtualapp import app,cursor,conn
 from flask import Flask, render_template,request,json,session,redirect,jsonify
 
-#from werkzeug import generate_password_hash, check_password_hash
+from werkzeug import generate_password_hash, check_password_hash
 #import os
 
 import inspect,random
-
+import numpy as np
 # A dummy class that will pretend to be a minimal PSLab . we shall use this for testing purposes.
 class dummy:
 	def __init__(self):
-		self.x=np.linspace(0,8*np.pi,samples)
+		self.x=np.linspace(0,8*np.pi,200)
 		self.r = random.random()
 	def capture1(self,chan,samples,tg):
 		'''
 		Example doc for capture command. returns a sine wave
 		'''
+		self.x=np.linspace(0,8*np.pi,samples)
 		return self.x,np.sin(self.x+np.pi*random.random()/10)
 	def capture2(self,samples,tg):
+		self.x=np.linspace(0,8*np.pi,samples)
 		return self.x,np.sin(self.x+np.pi*random.random()/10),np.sin(self.x+np.pi*random.random()/10)
 
 	def get_voltage(self,chan):
@@ -49,6 +51,39 @@ for a in dir(I):
 @app.route('/index')
 @app.route('/main')
 def index():
-    return render_template('index.html')
+	'''
+	Home Page link
+	'''
+	return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+	'''
+	Sign-up Page link
+	'''
+	return render_template('signup.html')
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+	# read the posted values from the UI
+	_name = request.form['inputName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+
+	# validate the received values
+	if _name and _email and _password:
+		_hashed_password = generate_password_hash(_password)
+		cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
+		print 'old ',_password,len(_password),'LENGTH : ',len(_hashed_password)
+		data = cursor.fetchall()		 
+		if len(data) is 0:
+			conn.commit()
+			return json.dumps({'message':'User %s created successfully. e-mail:%s !'%(_name,_email)})
+		else:
+			return json.dumps({'error':str(data[0][0])})
+	else:
+		return json.dumps({'error':'Fill all the required fields!'})
+
+
 
 
