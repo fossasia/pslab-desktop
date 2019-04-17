aa89e8417e1ba570b058b2bd3478f3b0dbf42db0
Shubham Gupta <shubhamg@outlook.com>
2019-04-17 17:29:06 +0530

    Add Settings for Oscilloscope instrument #238 (#239)

---
+++ src/screen/Oscilloscope/components/settingOptions.js
@@ -0,0 +1,42 @@
+export const options = {
+    "Range1": {
+        "16": "+/-16V", 
+        "8": "+/-8V",
+        "4": "+/-4V", 
+        "3": "+/-3V", 
+        "2": "+/-2V", 
+        "1": "+/-1V",
+        "0.5": "+/-500V",
+        "160": "+/-160V",
+    },
+    "Channel1": {
+        "CH1": "CH1", 
+        "CH2": "CH2",
+        "CH3": "CH3", 
+        "MIC": "MIC", 
+        "CAP": "CAP", 
+        "SEN": "SEN",
+        "AN8": "AN8",
+    },
+    "Mic": {
+        "Microphone": "Microphone",
+        "Inbuilt": "In-Built Mic"
+    },
+    "Select": {
+        "CH1": "CH1", 
+        "CH2": "CH2",
+        "CH3": "CH3", 
+        "MIC": "MIC",
+    },
+    "DataAnalysisSelect": {
+        "None": "None",
+        "CH1": "CH1", 
+        "CH2": "CH2",
+        "CH3": "CH3", 
+        "MIC": "MIC",
+    },
+    "FitSelect": {
+        "Sine": "Sine Fit",
+        "Square": "Square Fit"
+    }
+}
