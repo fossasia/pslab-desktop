52f366d68cd7c159f2070bccd11491d9586f8d2a
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-04-18 19:08:44 +0530

    Keep alive architecture implemented

--- src/App.js
+++ src/App.js
@@ -8,8 +8,29 @@ import LogicAnalyser from './screen/LogicAnalyser';
 import PowerSource from './screen/PowerSource';
 import Settings from './screen/Settings';
 import './App.css';
+const electron = window.require('electron');
+const ipcRenderer = electron.ipcRenderer;
+const loadBalancer = window.require('electron-load-balancer');
 
 class App extends Component {
+	componentDidMount() {
+		loadBalancer.startBackgroundProcess(ipcRenderer, 'oscilloscope', {
+			numberOfSamples: 1000,
+			timeGap: 10,
+			activeChannels: {
+				ch1: true,
+				ch2: false,
+				ch3: false,
+				ch4: false,
+			},
+			delay: 100,
+		});
+	}
+
+	componentWillUnmount() {
+		loadBalancer.stopBackgroundProcess(ipcRenderer, 'oscilloscope');
+	}
+
 	render() {
 		return (
 			<HashRouter>
