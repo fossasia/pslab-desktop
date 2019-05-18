0a4bb4faced78ca738fd80956b3e6a54136469ac
Ojas Gulati <ojasgulati100@gmail.com>
2019-05-19 00:49:41 +0530

    [#306] SVG to inlineSVG

---
+++ src/resources/CapacitorIcon.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const CapacitorIcon = ({ size = '1.5em', color = 'rgba(0, 0, 0, 0.38)' }) => {
+  return (
+    <svg
+      version="1.1"
+      id="Capa_1"
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width={size}
+      height={size}
+      viewBox="0 0 460 460"
+    >
+      <g fill={color} id="XMLID_1382_">
+        <polygon
+          id="XMLID_1383_"
+          points="460,215 314,215 314,50 284,50 284,410 314,410 314,245 460,245 	"
+        />
+        <polygon
+          id="XMLID_1384_"
+          points="146,215 0,215 0,245 146,245 146,410 176,410 176,50 146,50 	"
+        />
+      </g>
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+      <g />
+    </svg>
+  );
+};
+
+export default CapacitorIcon;
