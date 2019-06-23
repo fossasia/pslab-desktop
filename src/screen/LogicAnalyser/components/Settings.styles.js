2d8f2ee76702006c1bb2ba5590b2863dfc190ad0
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-06-23 14:11:38 +0530

    [#378] Logic analyser graph implemented

--- src/screen/LogicAnalyser/components/Settings.styles.js
+++ src/screen/LogicAnalyser/components/Settings.styles.js
@@ -24,10 +24,16 @@ export const OptionsRowWrapper = styled.div`
 export const GraphWrapper = styled.div`
   margin: 16px 0px 0px 16px;
   display: flex;
+  flex-direction: column;
   height: calc(100% - 16px);
   width: calc(100% - 16px);
   background-color: #ffffff;
   border: 1px solid #e8e8e8;
   color: rgba(0, 0, 0, 0.65);
   border-radius: 2px;
+`;
+
+export const ProgressWrapper = styled.div`
+  width: 100%;
+  height: 4px;
 `;
