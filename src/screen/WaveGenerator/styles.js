73629a09a7b19f2e9f4ccc026a157096f176c837
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-07-23 23:25:11 +0530

    [#437] Wave generator UI similar to Android completed

---
+++ src/screen/WaveGenerator/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: ${props => props.theme.background.default};
+`;
+
+export const Wrapper = styled.div``;
