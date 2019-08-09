188b343bde1728c4618609c96c1268a8f47a0342
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2019-08-09 23:09:04 +0530

    minmor modifications in playback python

--- scripts/multimeter_playback.py
+++ scripts/multimeter_playback.py
@@ -1,18 +1,33 @@
 import sys
+import threading
 
 
 class MulmetPlayback:
     def __init__(self):
         self.buffer = None
+        self.isReading = False
+        self.playback_thread = None
+        self.index = 0
 
     def read_data_from_file(self, data_path):
         print('Reading files')
         sys.stdout.flush()
 
     def start_playback(self):
-        print('starting playback')
-        sys.stdout.flush()
+        self.playback_thread = threading.Thread(
+            target=self.read_loop,
+            name='mulmet_playback')
 
     def stop_playback(self):
+        self.isReading = False
+        # join here
+        self.read_loop = None
         print('Stoping playback')
         sys.stdout.flush()
+
+    def read_loop(self):
+        # Also add index conditoin
+        while self.isReading:
+            # Read data and print
+            print('reading data - line')
+            sys.stdout.flush()
