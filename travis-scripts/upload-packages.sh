5d9c3957ebdd209bf5ed6fe1536b049fb64b4154
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-01-18 19:05:59 +0000

    chore: revert dropbox deployment (#590)

--- travis-scripts/upload-packages.sh
+++ travis-scripts/upload-packages.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
-curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o dropbox_uploader.sh
-chmod +x dropbox_uploader.sh
+git config --global user.name "Travis CI"
+git config --global user.email "noreply@travis.com"
 
-openssl aes-256-cbc -K $enc_key -iv $enc_iv -in .dropbox_uploader.enc -out ~/.dropbox_uploader -d
+git clone --quiet --branch=install https://fossasia:$GITHUB_API_KEY@github.com/fossasia/pslab-desktop install > /dev/null
+    
+cd install 
 
-find dist -maxdepth 1 -type f \( -name '*.snap' -o -name '*.deb' -o -name '*.exe' -o -name '*.dmg' \) -exec sh -c 'file=$(basename "{}");cp "{}" "temp-$file";' \;
+rm -rf pslab-desktop-$TRAVIS_BRANCH-$TRAVIS_JOB_NAME*
+
+find ../dist -maxdepth 1 -type f \( -name '*.snap' -o -name '*.deb' -o -name '*.exe' \) -exec sh -c 'file=$(basename "{}");cp "{}" "temp-$file";' \;
 
 for file in temp-*; do
     filename=$(basename -- "$file")
-    ./dropbox_uploader.sh upload "$file" "PsLab-Desktop/pslab-desktop-$TRAVIS_BRANCH-$TRAVIS_JOB_NAME.${filename##*.}"
+    mv "$file" "pslab-desktop-$TRAVIS_BRANCH-$TRAVIS_JOB_NAME.${filename##*.}"
 done
+
+git checkout --orphan temporary
+git add --all .
+git commit -am "[Auto] Update Install Packages ($(date +%Y-%m-%d.%H:%M:%S))"
+git branch -D install
+git branch -m install
+
+git push origin install --force --quiet > /dev/null
