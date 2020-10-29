b312217c01d84070b0cb82af81a2a23d27bb383c
Abhijay jain <abhijay007j@gmail.com>
2020-10-29 17:09:33 +0530

    Set up Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ]
+}
