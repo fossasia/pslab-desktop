ffae2cb7f95c73021d75c967536920135a686233
Daniel Maslowski <info@orangecms.org>
2020-10-31 01:52:07 +0100

    Add story for CustomDialog

---
+++ src/components/CustomDialog/CustomDialog.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { action, actions } from '@storybook/addon-actions';
+
+import CustomDialog from './CustomDialog';
+
+export default {
+  title: 'Components/CustomDialog',
+  component: CustomDialog,
+};
+
+export const SimpleDialog = () => (
+  <CustomDialog onDialogClose={action('close')} title="Hello" isOpen />
+);
+
+export const InputDialog = () => (
+  <CustomDialog
+    title="Hello"
+    isOpen
+    hint="hello :)"
+    variant="simple-input"
+    inputCheck={a => a.length > 0}
+    {...actions('onAccept', 'onCancel')}
+  />
+);
