7d2f24b0b2204c85775b94b3ed8939c97d8ce56b
[name] <55917516+AnshMishra2001@users.noreply.github.com>
2020-12-17 23:23:20 +0530

    package added

---
+++ package/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿
+$ErrorActionPreference = 'Stop'; 
+$packageName = 'package'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'PSLab-Desktop-Setup-2.5.2.exe'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  softwareName  = 'package*'
+  file          = $fileLocation
+  fileType      = 'EXE'
+   silentArgs   = '/S' 
+   validExitCodes= @(0)
+  checksum      = ''
+  checksumType  = 'sha256' 
+            
+ 
+   
+}
+
+Install-ChocolateyPackage @packageArgs 
+
+
