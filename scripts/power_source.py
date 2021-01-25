6886da5cbc4cf17a6d60b2098e7f04515d91deea
Alexander Bessman <alexander@bessman.se>
2021-01-25 22:32:35 +0100

    pslab-python 2.0

--- scripts/power_source.py
+++ scripts/power_source.py
@@ -10,17 +10,17 @@ class Power_source:
         self.power_supply = device.power_supply
 
     def set_config(self, pcs_value, pv1_value, pv2_value, pv3_value):
-        self.power_supply.pcs.current = pcs_value / 1000  # mA
-        self.power_supply.pv1.voltage = pv1_value
-        self.power_supply.pv2.voltage = pv2_value
-        self.power_supply.pv3.voltage = pv3_value
+        self.power_supply.pcs = pcs_value / 1000  # mA
+        self.power_supply.pv1 = pv1_value
+        self.power_supply.pv2 = pv2_value
+        self.power_supply.pv3 = pv3_value
 
     def get_config(self):
         output = {'type': 'GET_CONFIG_PWR_SRC',
-                  'pcs': self.power_supply.pcs.current,
-                  'pv1': self.power_supply.pv1.voltage,
-                  'pv2': self.power_supply.pv2.voltage,
-                  'pv3': self.power_supply.pv3.voltage}
+                  'pcs': self.power_supply.pcs,
+                  'pv1': self.power_supply.pv1,
+                  'pv2': self.power_supply.pv2,
+                  'pv3': self.power_supply.pv3}
         print(json.dumps(output))
         sys.stdout.flush()
 
@@ -32,5 +32,5 @@ class Power_source:
         timestamp = time.time()
         self.file_write.save_config(
             data_path, "PowerSource",  timestamp=timestamp, datetime=datetime_data,
-            pcs=self.power_supply.pcs.current, pv1=self.power_supply.pv1.voltage, pv2=self.power_supply.pv2.voltage,
-            pv3=self.power_supply.pv3.voltage)
+            pcs=self.power_supply.pcs, pv1=self.power_supply.pv1, pv2=self.power_supply.pv2,
+            pv3=self.power_supply.pv3)
